ce4de4c81351b0e3b90076425e378deee7bc2fab
Léo Colombaro <git@colombaro.fr>
2023-02-24 22:42:14 +0000

    Prepare enedis

--- enedis_exporter/__init__.py
+++ enedis_exporter/__init__.py
@@ -9,8 +9,11 @@ from sentry_sdk import capture_exception
 from influxdb_client import Point
 import influxdb_exporter
 
-from lowatt_enedis.api import API
-enedis = API(os.environ.get("ENEDIS_CLIENT_ID"), os.environ.get("ENEDIS_CLIENT_SECRET"))
+from sdk_enedis.api import API
+enedis = API(
+    os.environ.get("ENEDIS_CLIENT_ID"),
+    os.environ.get("ENEDIS_CLIENT_SECRET")
+)
 
 def fetch():
     today = date.today() - timedelta(days=1)
@@ -21,13 +24,13 @@ def fetch():
     try:
         for year in range(3):
             start = today.replace(year=today.year - year)
-            for releve in enedis.donnees_consos_informatives(
+            for releve in enedis.daily_consumption(
                 os.environ.get("PDL"),
                 from_date=(start - delta).isoformat(),
                 to_date=(start).isoformat()
             ):
                 conso = releve["consommation"]
-                points.append(Point("grdf")
+                points.append(Point("enedis")
                     .time(datetime.fromisoformat(conso["date_fin_consommation"]).replace(year=today.year))
                     .tag("year", start.year)
                     .field("energy", conso["energie"])
